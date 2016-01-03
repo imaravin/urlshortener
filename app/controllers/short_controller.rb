f73bb8b336f79d0209338f3e39f5fde74af87b1e
imaravin <aravinththangasami@gmail.com>
2016-01-03 09:08:46 +0530

    db

--- app/controllers/short_controller.rb
+++ app/controllers/short_controller.rb
@@ -1,11 +1,34 @@
 class ShortController < ApplicationController
+	ALPHABET ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMN\/%OPQRSTUVWXYZ0123456789".split(//)
+	attr_accessor:link
   def create
 	  	@value=params[:text]
 	  	u=Url.new
 	  	u.longurl=@value
-	  	u.shorturl="aravinth"
+	  	link=@value
+	  	u.shorturl=bijective_encode()
 	  	u.save
 	 	render :json => u 
   end
+
+	  def bijective_encode(i)
+	   
+			  return ALPHABET[0] if i == 0
+			  s = ''
+			  base = ALPHABET.length
+			  while i > 0
+			    s << ALPHABET[i.modulo(base)]
+			    i /= base
+			  end
+			  s.reverse
+	  end
+
+
+		def bijective_decode(s)
+		  i = 0
+		  base = ALPHABET.length
+		  s.each_char { |c| i = i * base + ALPHABET.index(c) }
+		  i
+		end
   
 end
